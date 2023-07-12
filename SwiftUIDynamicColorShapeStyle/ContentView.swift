e055b45edd88843850323df74e66e08bee85a71f
Kyle Lanchman <k.lanchman@gmail.com>
2023-07-11 23:23:05 -0400

    Initial Commit

---
+++ SwiftUIDynamicColorShapeStyle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIDynamicColorShapeStyle
+//
+//  Created by Kyle Lanchman on 7/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIDynamicColorShapeStyle/SwiftUIDynamicColorShapeStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDynamicColorShapeStyleApp.swift
+//  SwiftUIDynamicColorShapeStyle
+//
+//  Created by Kyle Lanchman on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDynamicColorShapeStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
